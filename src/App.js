24287bbb28174d7a91c40e6e8c528948cd09ab4b
Vishal062 <vs9425348@gmail.com>
2021-10-20 19:56:36 +0530

    React designer card done

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import { Pizza } from "./Components/pizzamenu";
+export default function App() {
+  return (
+    <div className="item">
+      <Pizza />
+    </div>
+  );
+}
